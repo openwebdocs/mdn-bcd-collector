bce29569ea041ba1fc60c6bc82e98fa76855cd54
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2023-03-31 11:44:11 -0700

    Resolve BCD and results dirs relative to project root

--- lib/config.ts
+++ lib/config.ts
@@ -8,10 +8,12 @@
 
 import {fileURLToPath} from 'node:url';
 
-export const BCD_DIR =
-  process.env.BCD_DIR ||
-  fileURLToPath(new URL(`../../browser-compat-data`, import.meta.url));
+export const BASE_DIR = new URL('..', import.meta.url);
 
-export const RESULTS_DIR =
-  process.env.RESULTS_DIR ||
-  fileURLToPath(new URL('../../mdn-bcd-results', import.meta.url));
+export const BCD_DIR = fileURLToPath(
+  new URL(process.env.BCD_DIR || '../browser-compat-data', BASE_DIR)
+);
+
+export const RESULTS_DIR = fileURLToPath(
+  new URL(process.env.RESULTS_DIR || '../mdn-bcd-results', BASE_DIR)
+);
