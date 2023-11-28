openwebdocs/mdn-bcd-collector

commit 7085386a3930a855a9d4815549693269fd06ea89

Ignore "Update overrides" commits in changelog
This filtering is already performed manually.

scripts/release.ts
@@ -212,7 +212,10 @@ const getGitChanges = async (ctx) => {
     await exec(`git log --pretty=format:%s v${currentVersion}..origin/main`),
   ).split("\n");
   ctx.commits = commits
-    .filter((summary) => !summary.startsWith("Bump "))
+    .filter(
+      (summary) =>
+        !summary.startsWith("Bump ") && summary.includes("Update overrides"),
+    )
     .map(
       (summary) =>
         `- ${summary.replaceAll("<", "&lt;").replaceAll(">", "&gt;")}`,

commit 1c8db9d67b2389684baf24e7265976132dbcf821

Fix filtering of commits in release notes
This fixes an issue that was introduced in 7085386a3930a855a9d4815549693269fd06ea89.

scripts/release.ts
@@ -214,7 +214,7 @@ const getGitChanges = async (ctx) => {
   ctx.commits = commits
     .filter(
       (summary) =>
-        !summary.startsWith("Bump ") && summary.includes("Update overrides"),
+        !(summary.startsWith("Bump ") || summary.includes("Update overrides")),
     )
     .map(
       (summary) =>