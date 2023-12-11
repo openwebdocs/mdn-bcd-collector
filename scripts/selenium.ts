21c0af7d3d7a1f456f4162f240bab37ca3abd349
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2023-12-11 08:57:09 -0800

    Fix Selenium script

--- scripts/selenium.ts
+++ scripts/selenium.ts
@@ -642,7 +642,7 @@ const run = async (
 
     log(task, "Exporting results...");
     await goToPage(driver, browser, version, `${host}/export`);
-    const downloadEl = await driver.findElement(By.id("export-download"));
+    const downloadEl = await driver.findElement(By.id("download"));
     const downloadUrl = await downloadEl.getAttribute("href");
 
     if (!ctx.testenv) {
