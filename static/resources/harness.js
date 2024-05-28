a9a09a68c6154c0d5ff7eb91655444005f0ebb3e
WATANABE Shinya <nabetti1720@gmail.com>
2024-05-28 17:56:14 +0900

    feat: Update constructor judgments of LLRT (#1472)

--- static/resources/harness.js
+++ static/resources/harness.js
@@ -316,7 +316,8 @@
           "must be callable",
           "is not a non-null object",
           "is not a function",
-          "Failed to construct" // Used by workerd
+          "Failed to construct", // Used by workerd
+          "Error calling function with"
         ])
       ) {
         // If it failed to construct and it's not illegal or just needs
