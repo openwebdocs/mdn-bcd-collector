e23d086d85dfa46d4bbb3e87e56513cc4fb99ff7
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2025-02-16 19:12:40 -0800

    Merge configuration files

--- eslint.config.js
+++ eslint.config.js
@@ -15,6 +15,11 @@ const compat = new FlatCompat({
   recommendedConfig: js.configs.recommended,
   allConfig: js.configs.all,
 });
+const staticCompat = new FlatCompat({
+  baseDirectory: __dirname + "/static",
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
 
 export default [
   {
@@ -32,7 +37,9 @@ export default [
     "plugin:@typescript-eslint/stylistic",
     "plugin:jsdoc/recommended-typescript",
   ),
+  ...staticCompat.extends("eslint:recommended", "plugin:jsdoc/recommended"),
   {
+    files: ["**/*.ts", "**/eslint.config.js"],
     plugins: {
       "@typescript-eslint": typescriptEslint,
       "prefer-arrow": preferArrow,
@@ -95,6 +102,80 @@ export default [
 
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-empty-function": "off",
+    },
+  },
+  {
+    files: ["**/static/**"],
+    languageOptions: {
+      ecmaVersion: 3,
+      sourceType: "script",
+      globals: {
+        ...globals.browser,
+        JSON: "readonly",
+        Promise: "readonly",
+      },
+    },
+
+    rules: {
+      "comma-dangle": ["error", "never"],
+      curly: ["error", "all"],
+      "guard-for-in": "off",
+      indent: "off",
+      "max-len": "off",
+      "no-invalid-this": "off",
+
+      "no-unused-vars": [
+        "error",
+        {
+          caughtErrors: "none",
+        },
+      ],
+
+      "no-var": "off",
+      "prefer-arrow/prefer-arrow-functions": "off",
+      quotes: "off",
+      "require-jsdoc": "off",
+      "space-before-function-paren": "off",
+      "jsdoc/check-param-names": "error",
+      "jsdoc/require-description": "warn",
+
+      "jsdoc/require-jsdoc": [
+        "warn",
+        {
+          require: {
+            ArrowFunctionExpression: true,
+            ClassDeclaration: true,
+            ClassExpression: true,
+            FunctionDeclaration: true,
+            FunctionExpression: true,
+            MethodDefinition: true,
+          },
+        },
+      ],
+
+      "jsdoc/require-param-type": "error",
+      "jsdoc/require-returns": "error",
+      "jsdoc/require-returns-type": "off",
+      "jsdoc/no-undefined-types": "error",
+      "jsdoc/require-yields": "error",
+    },
+  },
+  {
+    files: ["**/static/unittest/**.js"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      globals: {
+        ...globals.browser,
+        ...globals.mocha,
+      },
+    },
+  },
+  {
+    files: [
+      "**/static/resources/custom-tests/api/AudioWorkletNode/WhiteNoiseProcessor.js",
+    ],
+    languageOptions: {
+      ecmaVersion: 6,
     },
   },
 ];
