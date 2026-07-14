b7dc1e075b3391a65b23097828bf187f1934df80
Florian Scholz <fs@florianscholz.com>
2026-07-14 16:11:44 +0200

    Rename per Daniel's feedback

--- views/stats.ts
+++ views/stats.ts
@@ -17,34 +17,32 @@ const collectorData = getMissing(bcd, tests, "bcd-from-collector")[""];
 const coverageData = {
   bcd: {
     version: bcd.__meta.version,
-    counts: {
-      all: bcdData.all.all.length,
-      covered: bcdData.all.found.length,
-      uncovered_percentage: (
-        (bcdData.all.found.length / bcdData.all.all.length) *
-        100.0
-      ).toFixed(2),
-      untestable: bcdData.untestable.all.length,
-      testable_uncovered: bcdData.testable.missing.length,
-      testable_uncovered_percentage: (
-        (bcdData.testable.found.length / bcdData.testable.all.length) *
-        100.0
-      ).toFixed(2),
+    summary: {
+      key_count: bcdData.all.all.length,
+      covered_keys_count: bcdData.all.found.length,
+      uncovered_keys_ratio: (
+        bcdData.all.found.length / bcdData.all.all.length
+      ).toFixed(4),
+      untestable_keys_count: bcdData.untestable.all.length,
+      testable_uncovered_keys_count: bcdData.testable.missing.length,
+      testable_uncovered_keys_ratio: (
+        bcdData.testable.found.length / bcdData.testable.all.length
+      ).toFixed(4),
     },
     lists: {
-      all: bcdData.all.all,
-      covered: bcdData.all.found,
-      untestable: bcdData.untestable.all,
-      testable_uncovered: bcdData.testable.missing,
+      all_keys: bcdData.all.all,
+      covered_keys: bcdData.all.found,
+      untestable_keys: bcdData.untestable.all,
+      testable_uncovered_keys: bcdData.testable.missing,
     },
   },
   collector: {
     version: appVersion,
-    counts: {
-      early_features_not_in_bcd: collectorData.testable.missing.length,
+    summary: {
+      early_keys_not_in_bcd: collectorData.testable.missing.length,
     },
     lists: {
-      early_features_not_in_bcd: collectorData.testable.missing,
+      early_keys_not_in_bcd: collectorData.testable.missing,
     },
   },
 };
